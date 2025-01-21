fc9764997d675b2cd9e97ae458cb115287d86394
yoh <yohkuoria@gmail.com>
2025-01-21 17:06:21 +0900

    up

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 'use strict';
 // ハンバーガーメニュー
 
-$(function() {
-  $('.check-b').click(function() {
-      $('[name="menu-btn"]').prop('checked', false);
+// $(function() {
+//   $(".check-b").click(function() {
+//       $(".menu-btn").prop('checked', false);
+//   });
+// });
+
+  $(".check-b").click(function() {
+      $("#menu-btn").prop('checked', false);
   });
-});
 
+// {
+//   const menubtn = document.getElementById('menu-icon');
+//   const hamburger = document.querySelectorAll('.menu li a');
+//   const hamA =Array.from(hamburger);
+//   hamA.forEach(function(element){
+//     element.addEventListener('click',function(){
+//       menubtn.checked = false
+//     })
+//   })
 
+// }
 
 
 
